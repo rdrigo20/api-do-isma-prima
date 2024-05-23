180b73de77fbec3ed20eeb0a8132b1409a8fb0ac
rdrigo20 <101879920+rdrigo20@users.noreply.github.com>
2024-05-23 14:25:30 -0300

    Add files via upload

---
+++ apisma/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+$template_diretorio = get_template_directory();
+
+require_once($template_diretorio . "/custom-post-type/cards.php");
+require_once($template_diretorio . "/custom-post-type/news.php");
+require_once($template_diretorio . "/custom-post-type/events.php");
+require_once($template_diretorio . "/custom-post-type/carrousel.php");
+
+
+
+?>
